bd173dd7e698fd65a4ca63e01b91b580ff4e7caf
monkychilote <p.subiabre@gmail.com>
2024-08-01 21:19:07 -0400

    modificacion .json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://monkychilote.github.io/desafio17_ReactVite_1/",
+  base: "/desafio17_ReactVite_1",
 });
